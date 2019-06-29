1c29b818ad45ae6e57544955d4a70396f92ae0d0
Daniel López <d.lopez.1740@gmail.com>
2019-06-29 18:01:19 -0400

    Terminada las rutas y la consulta a los Apis correspondientes

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,12 +2,103 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
 
 class VehicleController extends Controller
 {
-    public function listVehicles($modelYear, $manufacturer, $model)
+    /**
+     * @param $modelYear
+     * @param $manufacturer
+     * @param $model
+     * @return array
+     */
+    public function getListVehicles($modelYear, $manufacturer, $model)
     {
+        $withRating = request('withRating') === 'true';
+        return $this->listVehicles($modelYear, $manufacturer, $model, $withRating);
+    }
 
+    /**
+     * @param Request $request
+     * @return array
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function postListVehicles(Request $request)
+    {
+        $this->validate($request, [
+            'modelYear' => 'required',
+            'manufacturer' => 'required',
+            'model' => 'required'
+        ]);
+
+        return $this->listVehicles($request->modelYear, $request->manufacturer, $request->model);
+    }
+
+    /**
+     * Metodo para unificar la consulta y la respuesta del Api
+     * @param $modelYear
+     * @param $manufacturer
+     * @param $model
+     * @param bool $withRating
+     * @return array
+     */
+    protected function listVehicles($modelYear, $manufacturer, $model, $withRating = false)
+    {
+        $url = "https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/{$modelYear}/make/{$manufacturer}/model/{$model}?format=json";
+        $resultsResponse = $this->connectionToApi($url);
+        $count = $resultsResponse->Count ?? 0;
+        $results = $resultsResponse->Results ?? [];
+
+        $response = [
+            'Count' => $count,
+            'Results' => []
+        ];
+        foreach ($results as $result) {
+            $response['Results'][] = $this->withRatingResults($result, $withRating);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Con este metodo se puede comprobar si los rating estan siendo solicitados
+     *
+     * @param $result
+     * @param bool $withRating
+     * @return array
+     */
+    private function withRatingResults($result, bool $withRating): array
+    {
+        $vehicleId = $result->VehicleId;
+        if ($withRating) {
+            $url = "https://one.nhtsa.gov/webapi/api/SafetyRatings/VehicleId/{$vehicleId}?format=json";
+            $response = $this->connectionToApi($url);
+            $resArr['CrashRating'] = $response->Results[0]->OverallRating ?? 'Not Rated';
+        }
+        $resArr['Description'] = $result->VehicleDescription;
+        $resArr['VehicleId'] = $vehicleId;
+
+        return $resArr;
+    }
+
+    /**
+     * Metodo para la coneccion al api desde php
+     * @param $url
+     * @return mixed
+     */
+    private function connectionToApi($url)
+    {
+        $jsonResponse = json_decode('');
+        $client = new Client();
+        try {
+            $responseClient = $client->request('GET', $url);
+            $jsonResponse = json_decode($responseClient->getBody()->getContents());
+        } catch (GuzzleException $e) {
+            //todo: respuesta para cuando exista un error en la consulta del Api
+        }
+
+        return $jsonResponse;
     }
 }
--- tests/Feature/ApiVehicleTest.php
+++ tests/Feature/ApiVehicleTest.php
@@ -3,17 +3,39 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ApiVehicleTest extends TestCase
 {
+    private $resulJson = [
+        'Count' => 4,
+        'Results' => [
+            ['Description' => '2015 Audi A3 4 DR AWD', 'VehicleId' => 9403],
+            ['Description' => '2015 Audi A3 4 DR FWD', 'VehicleId' => 9408],
+            ['Description' => '2015 Audi A3 C AWD', 'VehicleId' => 9405],
+            ['Description' => '2015 Audi A3 C FWD', 'VehicleId' => 9406]
+        ]
+    ];
     /**
      * @test
      */
-    public function list_vehicles()
+    public function get_list_vehicles()
     {
         $this->get('/vehicles/2015/Audi/A3')
-            ->assertStatus(200);
+            ->assertStatus(200)
+            ->assertJson($this->resulJson);
+    }
+
+    /**
+     * @test
+     */
+    public function post_list_vehicles()
+    {
+        $this->post('/vehicles', [
+            'modelYear' => 2015,
+            'manufacturer' => 'Audi',
+            'model' => 'A3'
+        ])
+            ->assertStatus(200)
+            ->assertJson($this->resulJson);
     }
 }

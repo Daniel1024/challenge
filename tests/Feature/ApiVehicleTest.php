e772c5b3e6ae138a878e0cf68ec14bb51363a168
Daniel López <d.lopez.1740@gmail.com>
2019-06-29 18:14:30 -0400

    Terminada las pruebas para la consultas del api externo

--- tests/Feature/ApiVehicleTest.php
+++ tests/Feature/ApiVehicleTest.php
@@ -15,6 +15,7 @@ class ApiVehicleTest extends TestCase
             ['Description' => '2015 Audi A3 C FWD', 'VehicleId' => 9406]
         ]
     ];
+
     /**
      * @test
      */
@@ -22,7 +23,38 @@ class ApiVehicleTest extends TestCase
     {
         $this->get('/vehicles/2015/Audi/A3')
             ->assertStatus(200)
-            ->assertJson($this->resulJson);
+            ->assertExactJson($this->resulJson);
+    }
+
+    /**
+     * @test
+     */
+    public function get_list_vehicles_without_results()
+    {
+        $this->get('/vehicles/2051/Audi/A3')
+            ->assertStatus(200)
+            ->assertExactJson([
+                'Count' => 0,
+                'Results' => []
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function get_list_vehicles_with_rating_true()
+    {
+        $this->get('/vehicles/2015/Audi/A3?withRating=true')
+            ->assertStatus(200)
+            ->assertExactJson([
+                'Count' => 4,
+                'Results' => [
+                    ['CrashRating' => '5', 'Description' => '2015 Audi A3 4 DR AWD', 'VehicleId' => 9403],
+                    ['CrashRating' => '5', 'Description' => '2015 Audi A3 4 DR FWD', 'VehicleId' => 9408],
+                    ['CrashRating' => 'Not Rated', 'Description' => '2015 Audi A3 C AWD', 'VehicleId' => 9405],
+                    ['CrashRating' => 'Not Rated', 'Description' => '2015 Audi A3 C FWD', 'VehicleId' => 9406]
+                ]
+            ]);
     }
 
     /**
@@ -36,6 +68,23 @@ class ApiVehicleTest extends TestCase
             'model' => 'A3'
         ])
             ->assertStatus(200)
-            ->assertJson($this->resulJson);
+            ->assertExactJson($this->resulJson);
+    }
+
+    /**
+     * @test
+     */
+    public function post_list_vehicles_without_results()
+    {
+        $this->post('/vehicles', [
+            'modelYear' => 2051,
+            'manufacturer' => 'Audi',
+            'model' => 'A3'
+        ])
+            ->assertStatus(200)
+            ->assertExactJson([
+                'Count' => 0,
+                'Results' => []
+            ]);
     }
 }
